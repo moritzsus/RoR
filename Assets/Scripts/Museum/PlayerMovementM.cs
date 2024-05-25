3fee1d48b074185ac4e1a79928cc797d563a581a
Moritz Süssel <suessel.moritz@web.de>
2024-05-25 19:53:35 +0200

    playerController in museum and lights

--- Assets/Scripts/Museum/PlayerMovementM.cs
+++ Assets/Scripts/Museum/PlayerMovementM.cs
@@ -5,34 +5,11 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement")]
     public float moveSpeed;
 
-    public float groundDrag;
-
-    public float jumpForce;
-    public float jumpCooldown;
     public float airMultiplier;
-    bool readyToJump;
-
-    [HideInInspector] public float walkSpeed;
-    [HideInInspector] public float sprintSpeed;
-
-    [Header("Keybinds")]
-    public KeyCode jumpKey = KeyCode.Space;
-
-    [Header("Ground Check")]
-    public float playerHeight;
-    public LayerMask whatIsGround;
-    bool grounded;
 
     public Transform orientation;
 
-    float horizontalInput;
-    float verticalInput;
-
-    Vector3 moveDirection;
-
-    Rigidbody rb;
-
-    private bool resetPosAfterMinigame = false;
+    private CharacterController controller;
 
     private void Awake()
     {
@@ -41,15 +18,11 @@ public class PlayerMovement : MonoBehaviour
         Vector3 savedRotation = MainManager.GetInstance().GetLastPlayerRotation();
         transform.position = savedPosition;
         orientation.rotation = Quaternion.Euler(savedRotation);
-        resetPosAfterMinigame = false;
     }
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
-
-        readyToJump = true;
+        controller = GetComponent<CharacterController>();
     }
 
     private void Update()
@@ -57,87 +30,21 @@ public class PlayerMovement : MonoBehaviour
         if (!GameManagerMuseum.GetInstance().GetIsGameRunning())
             return;
 
-        if (!resetPosAfterMinigame)
-        {
-            resetPosAfterMinigame = true;
-            Vector3 savedPosition = MainManager.GetInstance().GetLastPlayerPosition();
-            Vector3 savedRotation = MainManager.GetInstance().GetLastPlayerRotation();
-            transform.position = savedPosition;
-            orientation.rotation = Quaternion.Euler(savedRotation);
-        }
-        // ground check
-        grounded = Physics.Raycast(transform.position, Vector3.down, (playerHeight * 0.5f) + 0.3f, whatIsGround);
-
         MyInput();
-        SpeedControl();
-
-        // handle drag
-        if (grounded)
-            rb.drag = groundDrag;
-        else
-            rb.drag = 0;
-    }
-
-    private void FixedUpdate()
-    {
-        if (!GameManagerMuseum.GetInstance().GetIsGameRunning())
-            return;
-
-        MovePlayer();
     }
 
     private void MyInput()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
 
-        // when to jump
-        if (Input.GetKey(jumpKey) && readyToJump && grounded)
-        {
-            readyToJump = false;
+        Vector3 moveDirection = (orientation.forward * verticalInput) + (orientation.right * horizontalInput);
 
-            Jump();
-
-            Invoke(nameof(ResetJump), jumpCooldown);
-        }
+        MovePlayer(moveDirection.normalized);
     }
 
-    private void MovePlayer()
+    private void MovePlayer(Vector3 moveDirection)
     {
-        // calculate movement direction
-        moveDirection = (orientation.forward * verticalInput) + (orientation.right * horizontalInput);
-
-        // on ground
-        if (grounded)
-            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
-
-        // in air
-        else if (!grounded)
-            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
-    }
-
-    private void SpeedControl()
-    {
-        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-
-        // limit velocity if needed
-        if (flatVel.magnitude > moveSpeed)
-        {
-            Vector3 limitedVel = flatVel.normalized * moveSpeed;
-            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
-        }
-    }
-
-    private void Jump()
-    {
-        // reset y velocity
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-
-        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-    }
-
-    private void ResetJump()
-    {
-        readyToJump = true;
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
     }
 }
